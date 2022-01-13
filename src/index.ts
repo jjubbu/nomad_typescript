e4ee43e4d64d91822fc475cb0df4f5011157fde4
jjubbu <hogugugut4102.off@gmail.com>
2022-01-13 23:34:49 +0900

    #0.7

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,19 @@ const sayHi = (hum:Hum):string=> {
 }
 
 console.log(sayHi(human))
+class Humm {
+    public name:string;
+    public age:number;
+    constructor (name:string, age:number){
+        // 이 클래스의 name 과 argument name 은 같다..는 으미!
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const seona = new Humm("jjubbu",24);
+
+console.log(sayHi(seona))
+
 
 export {};
